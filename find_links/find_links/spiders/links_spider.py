a85564ea44d84e6fb5d4952b0d5b4d1b4550ad07
pyRadiohead <y.kovalenko@live.com>
2018-11-06 14:39:55 +0200

    updated spider

--- find_links/find_links/spiders/links_spider.py
+++ find_links/find_links/spiders/links_spider.py
@@ -4,11 +4,35 @@ website_list = [i.strip() for i in f]
 
 
 
-class QuotesSpider(scrapy.Spider):
+class LinksFinder(scrapy.Spider):
     name = "find_links"
-    start_urls = website_list
+    start_urls = ['http://15krace.co.nz/',]
+    current_website =''
+    # custom_settings = {
+    #     # specifies exported fields and order
+    #     'FEED_EXPORT_FIELDS': [
+    #        'website',
+    #         'link1',
+    #         'link2',
+    #         'link3',
+    #     ],
+    # }
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        self.current_website = response.url
+        links = response.xpath('*//a/@href').extract()
+        for link in links:
+            if link.startswith(response.url) and len(link) > len(response.url):
+                print('Link start with', link)
+                yield response.follow(link,self.parse_internal_links)
+            pass
+
+    def parse_internal_links(self, response):
+            links = response.xpath('*//a/@href').extract()
+            if links is not None:
+                for link in links:
+                    if link != self.current_website:
+                        print(link)
+            else:
+                pass
+
+
