5a89885e2ba426715828216735126ca345d823c0
pyRadiohead <y.kovalenko@live.com>
2018-11-07 17:30:44 +0200

    updated spider

--- find_links/find_links/spiders/links_spider.py
+++ find_links/find_links/spiders/links_spider.py
@@ -1,48 +1,49 @@
 import scrapy
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
+
 f = open('website_list.txt', 'r')
-website_list = [i.strip() for i in f]
-no_need_links =['twitter','facebook','pinterest','mythemeshop','google','respond','mailto','#']
+# website_list = ['http://'+i.strip() for i in f]
+l = open('link_list.txt', 'r')
+link_list = [i.strip() for i in l]
 
-
+no_need_links =['twitter','facebook','pinterest','mythemeshop','google','respond','mailto']
+key_words =['casino', 'gambling', 'poker','bet','lucky', 'gambl']
 
 class LinksFinder(scrapy.Spider):
     name = "find_links"
-    start_urls = ['http://15krace.co.nz/',]
+    website_urls = ['http://tuplajaat.net','http://tt-kreis-pforzheim.de','http://otahunariding.co.nz','http://tarpilenskennel.com.preview.internetvikings.com']
     current_website =''
-    # custom_settings = {
-    #     # specifies exported fields and order
-    #     'FEED_EXPORT_FIELDS': [
-    #        'website',
-    #         'link1',
-    #         'link2',
-    #         'link3',
-    #     ],
-    # }
+
+    def start_requests(self):
+        for website in self.website_urls:
+            yield scrapy.Request(website,self.parse)
+        
     def link_filter(self,link_for_check):
-        for link in no_need_links:
-            if link in link_for_check:
-                print(link +' not in ' + link_for_check)
-                return False
-            return True
+        if not link_for_check.startswith(self.current_website):
+            for i in link_list:
+                if i in link_for_check:
+                        return True
+            return False
+        return False
 
     def parse(self, response):
         self.current_website = response.url
         links = response.xpath('*//a/@href').extract()
         for link in links:
             if link.startswith(response.url) and len(link) > len(response.url):
-                print('Link start with', link)
                 yield response.follow(link,self.parse_internal_links)
             pass
 
     def parse_internal_links(self, response):
-            links = response.xpath('*//a/@href').extract()
-            links = filter(self.link_filter,links)
-            if links is not None:
-                for link in links:
-                    for i in no_need_links:
-                        if not link.startswith(self.current_website):
-                            print(link)
-            else:
-                pass
+        links = response.xpath('*//a/@href').extract()
+        if links is not None:
+            links = filter(self.link_filter, links)
+            for link in links:
+                print(response.url,link)
+        else:
+             pass
+
 
 
